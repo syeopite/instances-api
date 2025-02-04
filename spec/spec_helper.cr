fc062cdfcfd346816a56fa3894d08a853ef06feb
syeopite <syeopite@syeopite.dev>
2025-02-04 11:07:30 -0800

    Ensure HTTP::Client requests always fail in tests
    Injected mock services can sometimes stop working due to upstream bugs
    
    This commit ensures that even if the mock services fail to get injected
    the HTTP requests done by the actual services will always fail ensuring
    a failure condition in the tests.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -13,3 +13,21 @@ require "../src/extract.cr"
 require "../src/populate.cr"
 
 Config = InstancesApi::YamlConfig.from_yaml("")
+
+
+# Ensure HTTP::Client is never called
+#
+# Taken from https://github.com/manastech/webmock.cr/blob/42b347cdd64e13193e46167a03593944ae2b3d20/src/webmock/core_ext.cr#L20
+class HTTP::Client
+  private def exec_internal(request : HTTP::Request)
+    exec_internal(request, &.itself).tap do |response|
+      response.consume_body_io
+      response.headers.delete("Transfer-encoding")
+      response.headers["Content-length"] = response.body.bytesize.to_s
+    end
+  end
+
+  private def exec_internal(request, &block : Response -> T) : T forall T
+    return HTTP::Client::Response.new(status_code=403)
+  end
+end
